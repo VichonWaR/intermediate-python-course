9b05dc5d045ecb738a5b3e2b241cf9e958da8f23
Vichon Jward <vichonjward@gmail.com>
2020-03-18 17:06:09 -0700

    add user input and doubblesoutput alg

--- dice_roller.py
+++ dice_roller.py
@@ -1,18 +1,15 @@
 import random
 
-
-
-
-
-
 def main():
-    dice_rolls = 2
+    dice_rolls = int(input("How many dice would you like to roll? "))
     dice_sum = 0
     for i in range(0,dice_rolls):
         roll = random.randint(1,6)
         dice_sum += roll
         print(f'You rolled a {roll}')
-    print(f'a total of {dice_sum}')
+    if dice_sum - roll == roll:
+        print('Doubles!')
+    print(f'A total of {dice_sum}')
 
 if __name__== "__main__":
   main()
