df22836f91c359d7c4672bfdc639599e285c78af
Vichon Jward <vichonjward@gmail.com>
2020-04-05 08:05:49 -0700

    add 'ENTER to exit' command for cmd

--- dice_roller.py
+++ dice_roller.py
@@ -3,13 +3,16 @@ import random
 def main():
     dice_rolls = int(input("How many dice would you like to roll? "))
     dice_sum = 0
+    dice_size = int(input('How many sides are on the dice? '))
+    
     for i in range(0,dice_rolls):
-        roll = random.randint(1,6)
+        roll = random.randint(1,dice_size)
         dice_sum += roll
         print(f'You rolled a {roll}')
     if dice_sum - roll == roll:
         print('Doubles!')
     print(f'A total of {dice_sum}')
+    input('Press ENTER to exit!')
 
 if __name__== "__main__":
   main()
